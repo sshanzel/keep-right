26e200d639e1d6a08758a73c2f805be117bdcf1a
Hansel Solevilla <sshanzel@yahoo.com>
2020-02-28 10:52:42 +0400

    feat: Entities package and its fields

--- domain/entities/something.go
+++ domain/entities/something.go
@@ -1,6 +1,19 @@
 package entities
 
+import (
+	"github.com/google/uuid"
+)
+
 // Something that you have be it a container or an item
 type Something struct {
-	Title string
+	ID          uuid.UUID
+	Title       string
+	Description string
+}
+
+// NewSomething creates a new instance of Something!
+func NewSomething(title, description string) *Something {
+	s := Something{uuid.New(), title, description}
+
+	return &s
 }
--- domain/entities/user.go
+++ domain/entities/user.go
@@ -1,6 +1,20 @@
 package entities
 
+import (
+	"github.com/google/uuid"
+)
+
 // User information
 type User struct {
+	ID        uuid.UUID
 	Firstname string
+	Lastname  string
+	Email     string
+}
+
+// NewUser return a new instance of User
+func NewUser(firstname, lastname, email string) *User {
+	u := User{uuid.New(), firstname, lastname, email}
+
+	return &u
 }
