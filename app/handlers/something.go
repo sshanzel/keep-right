822552f86a76d869925c21d05ceb310a48f7f9d7
Hansel Solevilla <sshanzel@yahoo.com>
2020-10-19 16:15:58 +0400

    feat: Update Something

--- app/handlers/something.go
+++ app/handlers/something.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo"
 	"github.com/sshanzel/keep-right/app/dto"
 	"github.com/sshanzel/keep-right/domain/entities"
@@ -26,14 +27,14 @@ func NewSomething(c echo.Context) error {
 
 	smthng := new(dto.Something)
 
-	if err = c.Bind(smthng); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
-	}
-
 	user, err := _iurepo.GetUserByUID(token.UID)
 
 	if err != nil {
 		return c.JSON(http.StatusNotFound, "User not found!")
+	}
+
+	if err = c.Bind(smthng); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
 	}
 
 	newSmthng := entities.NewSomething(smthng.Title, smthng.Description, smthng.InsideOfID, smthng.SomethingTypeID, user.ID)
@@ -41,6 +42,32 @@ func NewSomething(c echo.Context) error {
 	_isrepo.CreateSomething(newSmthng)
 
 	return c.JSON(http.StatusOK, newSmthng)
+}
+
+// UpdateSomething is the handler for POSTing Something
+func UpdateSomething(c echo.Context) error {
+	id, err := uuid.Parse(c.Param("id"))
+	token, err := GetFirebaseToken(c.Request())
+
+	smthng := _isrepo.GetSomething(id)
+
+	user, err := _iurepo.GetUserByUID(token.UID)
+
+	if err != nil {
+		return c.JSON(http.StatusNotFound, "User not found!")
+	}
+
+	if user.ID != smthng.CreatedByID {
+		return c.JSON(http.StatusForbidden, "Action not allowed!")
+	}
+
+	if err = c.Bind(smthng); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	_isrepo.UpdateSomething(smthng)
+
+	return c.JSON(http.StatusOK, smthng)
 }
 
 // GetSomethings is the handler for POSTing Something
